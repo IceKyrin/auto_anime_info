9e080f35faa51830f16483c48455d3b65cc60519
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-06-19 15:18:34 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tmdb
 from b_spider import Bspider
 
 if __name__ == "__main__":
-    api_key = os.environ["SCKEY"]
+    api_key = os.environ["api_key"]
     Bspider(10)
     tmdb.run(380000, api_key)
     analysis.update(380000)
